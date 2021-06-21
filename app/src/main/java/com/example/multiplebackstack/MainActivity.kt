b3c088262b5ca4a35140d4ee4925b94ba612675d
Rina Thakkar <rina.mindinventory@gmail.com>
2021-06-21 19:10:31 +0530

    first commit

---
+++ app/src/main/java/com/example/multiplebackstack/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.multiplebackstack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        val navController = findNavController(R.id.fragNavHost)
+        bottomNav.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/multiplebackstack/post/Fragment5.kt
@@ -0,0 +1,31 @@
+package com.example.multiplebackstack.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.multiplebackstack.R
+import kotlinx.android.synthetic.main.fragment_1.view.*
+
+
+class Fragment5 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_5, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.button.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment5_to_fragment6)
+        }
+    }
+
+}
